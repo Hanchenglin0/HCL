75f4fcec19b27e64b6a1ee98ec9816798ce46e53
Hanchenglin0 <704530196@qq.com>
2015-10-09 14:19:53 +0800

    cron test

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,4 +1,5 @@
 node 'devops172303142', 'devops172303141', 'devops172303143' {
+	include puppet
 	file {
 		'/tmp/hello':
 		content => "hello ,world\n",
---
+++ modules/puppet/manifests/init.pp
@@ -0,0 +1,25 @@
+class puppet {
+	file {
+	'/usr/local/bin/papply':
+	source => 'puppet:///modules/puppet/papply.sh',
+	mode => '0755',
+}
+file {
+	'/usr/local/bin/pull-updates':
+	source => 'puppet:///modules/puppet/pull-updates.sh',
+	mode => '0755',
+}
+file {
+	'/root/.ssh/id_rsa':
+	source => 'puppet:///modules/puppet/root',
+	mode => '0600',
+}
+cron {
+	'run-puppet':
+	ensure => present,
+	user => 'root',
+	command => '/usr/local/bin/pull-updates',
+	minute => '*/1',
+	hour => '*',
+}
+}
