b898073ee74602fade53be3d774ff3671f97e8e8
Hanchenglin0 <704530196@qq.com>
2015-10-09 19:33:13 +0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,3 @@
-node 'devops172303142', 'devops172303141', 'devops172303143'{
-	include puppet
-	file {
-		'/tmp/hello':
-		content => "hello ,world\n",
-	}
-
+node 'devops172303142', 'devops172303141'{
+  include puppet
 }
