7ea67d923fe687c8af2b96c74ee0e658a2a42479
Hanchenglin0 <704530196@qq.com>
2015-10-09 19:28:01 +0800

    memcached

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,7 @@
 node 'devops172303142', 'devops172303141'{
-	include puppet
-	file {
-		'/tmp/hello':
-		content => "hello ,world\n",
-	}
-
+#  include puppet
+  include memcached
+#  file { '/tmp/hello':
+#    content => "hello ,world\n",
 }
+#}
